310bb11053554abf4029e59218e8657e1e6b0a4f
guoyongjie <guoyongjie@sunmi.com>
2021-02-04 10:46:54 +0800

    feat: 去掉portal

--- dist/PageScaffold.d.ts
+++ dist/PageScaffold.d.ts
@@ -1,10 +1,10 @@
 import React from 'react';
-export interface PageScaffoldProps {
+interface PageScaffoldProps {
     child?: React.ReactChild;
     children?: React.ReactNode;
     className?: string;
-    portal?: React.ReactElement;
     backgroundColor?: string;
     contrastingColor?: string;
 }
 export declare const PageScaffold: (props: PageScaffoldProps) => JSX.Element;
+export {};
--- src/PageScaffold.tsx
+++ src/PageScaffold.tsx
@@ -1,24 +1,14 @@
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
 
-export interface PageScaffoldProps {
+interface PageScaffoldProps {
   child?: React.ReactChild
   children?: React.ReactNode
   className?: string
-  portal?: React.ReactElement
   backgroundColor?: string
   contrastingColor?: string
 }
 
 export const PageScaffold = (props: PageScaffoldProps) => {
-  useEffect(() => {
-    if (props.portal) {
-      const portalContainer = document.createElement('div')
-      document.body.appendChild(portalContainer)
-      ReactDOM.render(props.portal, portalContainer)
-    }
-  }, [props.portal])
-
   return (
     <>
       <style>{`body { background: ${props.backgroundColor}; color: ${props.contrastingColor}; }`}</style>
